e39441c52d2cfa239b2a8bbbdf1e89599d0d4c72
idsc3101 <idsc3101@gmail.com>
2013-10-07 14:55:42 -0500

    Solved the first two problems in class.

--- FirstNameFunc/FirstNameFunc/Form1.Designer.vb
+++ FirstNameFunc/FirstNameFunc/Form1.Designer.vb
@@ -31,56 +31,62 @@ Partial Class Form1
         '
         'txtFullName
         '
-        Me.txtFullName.Location = New System.Drawing.Point(101, 21)
+        Me.txtFullName.Location = New System.Drawing.Point(76, 17)
+        Me.txtFullName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtFullName.Name = "txtFullName"
-        Me.txtFullName.Size = New System.Drawing.Size(201, 22)
+        Me.txtFullName.Size = New System.Drawing.Size(152, 20)
         Me.txtFullName.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(46, 24)
+        Me.Label1.Location = New System.Drawing.Point(34, 20)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(49, 17)
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Name:"
         '
         'btnDetermine
         '
-        Me.btnDetermine.Location = New System.Drawing.Point(101, 64)
+        Me.btnDetermine.Location = New System.Drawing.Point(76, 52)
+        Me.btnDetermine.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnDetermine.Name = "btnDetermine"
-        Me.btnDetermine.Size = New System.Drawing.Size(169, 39)
+        Me.btnDetermine.Size = New System.Drawing.Size(127, 32)
         Me.btnDetermine.TabIndex = 2
         Me.btnDetermine.Text = "Determine First Name"
         Me.btnDetermine.UseVisualStyleBackColor = True
         '
         'txtFirstName
         '
-        Me.txtFirstName.Location = New System.Drawing.Point(101, 128)
+        Me.txtFirstName.Location = New System.Drawing.Point(76, 104)
+        Me.txtFirstName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtFirstName.Name = "txtFirstName"
         Me.txtFirstName.ReadOnly = True
-        Me.txtFirstName.Size = New System.Drawing.Size(201, 22)
+        Me.txtFirstName.Size = New System.Drawing.Size(152, 20)
         Me.txtFirstName.TabIndex = 3
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(15, 131)
+        Me.Label2.Location = New System.Drawing.Point(11, 106)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(80, 17)
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "First Name:"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(356, 197)
+        Me.ClientSize = New System.Drawing.Size(267, 160)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.txtFirstName)
         Me.Controls.Add(Me.btnDetermine)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtFullName)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Extract First Name"
         Me.ResumeLayout(False)
